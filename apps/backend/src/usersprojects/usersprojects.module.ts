28bc9e4bd1838a7805d23d9718e26e3f94b41107
sebasjc19 <sebastian.jaramilloc@uqvirtual.edu.co>
2025-10-06 18:16:18 -0500

    Fix: export from module

--- apps/backend/src/usersprojects/usersprojects.module.ts
+++ apps/backend/src/usersprojects/usersprojects.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersprojectsService } from './usersprojects.service';
 import { UsersprojectsController } from './usersprojects.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserProject } from './entities/usersproject.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([UserProject
+    ]) 
+  ],
   controllers: [UsersprojectsController],
   providers: [UsersprojectsService],
+  exports: [UsersprojectsService],
 })
 export class UsersprojectsModule {}
