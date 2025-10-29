aae1dc947a8dabdbe554264df3db734a9041e88b
sebasjc19 <sebastian.jaramilloc@uqvirtual.edu.co>
2025-10-29 15:07:49 -0500

    Fix: add validations

--- apps/backend/src/usersprojects/dto/create-usersproject.dto.ts
+++ apps/backend/src/usersprojects/dto/create-usersproject.dto.ts
@@ -1,27 +1,34 @@
-import { IsNotEmpty, IsNumber } from "class-validator";
+import { IsEnum, IsNotEmpty, IsNumber } from "class-validator";
 import { UserRole } from "../enums/user-role.enum";
 import { ApiProperty } from "@nestjs/swagger";
-
-
+/**
+ * DTO for creating a new user-project relation.
+ */
 export class CreateUsersprojectDto {
+    /** User id */
     @ApiProperty({
             example: '1',
             description: 'The id of the user.',
     })
     @IsNumber()
+    @IsNotEmpty()
     userId: number;
 
+    /** Project id */
     @ApiProperty({
         example: '1',
         description: 'The id of the project.',
     })
     @IsNumber()
+    @IsNotEmpty()
     projectId: number;
 
+    /** Role of the user within the project */
     @ApiProperty({
         example: 'member',
         description: 'The role of the user in the project.',
     })
+    @IsEnum(UserRole)
     @IsNotEmpty()
     role: UserRole;
 }
--- apps/backend/src/usersprojects/dto/update-usersproject.dto.ts
+++ apps/backend/src/usersprojects/dto/update-usersproject.dto.ts
@@ -1,28 +1,18 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUsersprojectDto } from './create-usersproject.dto';
-import { IsNumber, IsNotEmpty } from 'class-validator';
+import { IsNumber, IsNotEmpty, IsEnum } from 'class-validator';
 import { UserRole } from '../enums/user-role.enum';
 import { ApiProperty } from '@nestjs/swagger';
-
+/**
+ * DTO for updating an existing user-project relation.
+ */
 export class UpdateUsersprojectDto{
+    /** User role updated */
     @ApiProperty({
-            example: '1',
-            description: 'The id of the user.',
-    })
-    @IsNumber()
-    userId: number;
-
-    @ApiProperty({
-        example: '1',
-        description: 'The id of the project.',
-    })
-    @IsNumber()
-    projectId: number;
-
-    @ApiProperty({
-        example: 'admin',
+        example: UserRole.ADMIN,
         description: 'The new role of the user in the project.',
     })
+    @IsEnum(UserRole)
     @IsNotEmpty()
     role: UserRole;
 }
