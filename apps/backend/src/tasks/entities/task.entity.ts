c642cb8afaa02f633d91f1b6e48ec74818cebbc2
sebasjc19 <sebastian.jaramilloc@uqvirtual.edu.co>
2025-10-29 13:25:26 -0500

    fix: improvement on documentation

--- apps/backend/src/tasks/entities/task.entity.ts
+++ apps/backend/src/tasks/entities/task.entity.ts
@@ -2,18 +2,24 @@ import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, ManyToMany, JoinTabl
 import { Project } from '../../projects/entities/project.entity';
 import { User } from '../../users/entities/user.entity';
 import { TaskState } from '../enums/task-state.enum';
-
+/**
+ * Task entity representing a single task within a project.
+ */
 @Entity()
 export class Task {
+  /** Task unique identifier. */
   @PrimaryGeneratedColumn()
   id: number;
 
+  /** Short title of the task. */
   @Column()
   title: string;
 
-  @Column()
+  /** Optional deatiled project description. */
+  @Column({ nullable: true })
   description: string;
 
+  /** Current state of the task. */
   @Column({
     type: 'enum',
     enum: TaskState,
@@ -21,15 +27,19 @@ export class Task {
   })
   state: TaskState;
 
+  /** Start date of the task. */
   @Column()
   startDate: Date;
 
+  /** Expected or actual end date. */
   @Column()
   endDate: Date;
 
+  /** The project to which this task belongs. */
   @ManyToOne(() => Project, project => project.tasks, { onDelete: 'CASCADE' })
   project: Project;
 
+  /** Users assigned to work on this task. */
   @ManyToMany(() => User, user => user.tasks)
   @JoinTable()
   assignedUsers: User[];
--- apps/backend/src/tasks/enums/task-state.enum.ts
+++ apps/backend/src/tasks/enums/task-state.enum.ts
@@ -1,5 +1,11 @@
+/**
+ * Defines the possible statuses of a task.
+ */
 export enum TaskState {
+    /** The task is pending to do. */
     TODO = 'TODO',
+    /** The task is currently going on */
     INPROGRESS = 'INPROGRESS',
+    /** The task is completed */
     DONE = 'DONE',
 }
