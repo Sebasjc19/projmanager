8031ccc0ae51c353ae0aa2c7e37440d818fc8e84
sebasjc19 <sebastian.jaramilloc@uqvirtual.edu.co>
2025-10-29 13:14:10 -0500

    Feat: adding forward ref

--- apps/backend/src/projects/projects.module.ts
+++ apps/backend/src/projects/projects.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity';
 import { UsersprojectsModule } from 'src/usersprojects/usersprojects.module';
 import { Task } from 'src/tasks/entities/task.entity';
+import { TasksModule } from 'src/tasks/tasks.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Project, Task]),
-    forwardRef(()=> UsersprojectsModule)
+    forwardRef(()=> UsersprojectsModule),
+    forwardRef(()=>TasksModule)
   ],
   controllers: [ProjectsController],
   providers: [ProjectsService],
