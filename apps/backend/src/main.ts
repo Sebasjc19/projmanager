c54761ece37a9fc18fd1dd19acc71e3097085d48
sebasjc19 <sebastian.jaramilloc@uqvirtual.edu.co>
2025-11-01 19:39:44 -0500

    fix(main): fix cors limitation

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -40,6 +40,13 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
 
   const port = process.env.PORT ?? 8080;
+
+    app.enableCors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    });
   await app.listen(port);
 }
 void bootstrap();
